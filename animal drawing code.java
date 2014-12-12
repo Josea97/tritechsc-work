90f0ab7ab4144d0de7e5ded9b2dfd30b0ea9e01b
Josea97 <joselamezcua97@gmail.com>
2014-12-12 09:39:56 -0800

    Create animal drawing code.java

---
+++ animal drawing code.java
@@ -0,0 +1,31 @@
+stroke(242, 24, 24);
+fill(18, 230, 32); //Body
+ellipse(200, 180, 100, 108);
+
+fill(255, 0, 0);
+ellipse(180, 100, 2, 70); //Antenas
+ellipse(220, 100, 2, 70); 
+
+fill(18, 230, 32); //Head
+ellipse(200, 116, 64, 55);
+
+
+fill(255, 0, 0); //Legs
+ellipse(180, 270, 2, 90);
+ellipse(220, 270, 2, 90);
+ellipse(200, 270, 2, 90);
+
+
+fill(255, 0, 0); //Arms
+ellipse(150, 210, 2, 80);
+ellipse(250, 210, 2, 80);
+
+
+fill(245, 240, 240); //Eye
+ellipse(200, 116, 25, 25);
+fill(23, 21, 21);
+ellipse(200, 116, 19, 19);
+
+stroke(0, 0, 0); //Mouth
+fill(255, 0, 0); 
+ellipse(200, 160, 60, 25);
